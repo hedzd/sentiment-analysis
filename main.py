d15aefe980226e500c6187022354bda820cd4b73
hedzd <hediehpourghasem@gmail.com>
2021-07-13 18:26:16 +0430

    get input

---
+++ main.py
@@ -0,0 +1,13 @@
+
+class AIagent:
+
+    def check_comment(self):
+        return True
+
+
+if __name__ == '__main__':
+    while True:
+        comment = input()
+        if comment == '!q':
+            break
+        # TODO: IMPLEMENT ALGORITHM
