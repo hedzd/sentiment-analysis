5fd812937fab39937dc345476614737e53fc178f
hedzd <hediehpourghasem@gmail.com>
2021-07-14 15:08:57 +0430

    train model

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ class SentimentAlgorithm:
         self.negDict = {}
         self.posDictTwoWords = {}
         self.negDictTwoWords = {}
+        self.posPwi = {}
+        self.negPwi = {}
+        self.posPpairwords = {}
+        self.negPpairwords = {}
         self.trainSentencesPos = []
         self.testSentencesPos = []
         self.trainSentencesNeg = []
@@ -15,7 +19,9 @@ class SentimentAlgorithm:
         self.read_files(pathPos, pathNeg)
         self.split_train_test()
         self.make_dict(True)
-        self.clean_dict()
+        # self.clean_dict()
+        self.sumValuesPos = sum(self.posDict.values())
+        self.sumValuesNeg = sum(self.negDict.values())
 
     def read_files(self, pathPos, pathNeg):
         with open(pathPos, encoding="utf8") as reader:
@@ -42,6 +48,8 @@ class SentimentAlgorithm:
         del self.trainSentencesNeg[0:num_test_neg]
 
     def make_dict(self, bigram):
+        self.posDict['<s>'] = len(self.trainSentencesPos)
+        self.posDict['</s>'] = len(self.trainSentencesPos)
         for line in self.trainSentencesPos:
             line = line.split()
             # print(line)
@@ -53,7 +61,7 @@ class SentimentAlgorithm:
                     self.posDict[word] = 1
                 if bigram:
                     if i == len(line) - 1:
-                        twoWords = word + ' <>'
+                        twoWords = word + ' </s>'
                     else:
                         twoWords = word + ' ' + line[i + 1].strip("'")
 
@@ -61,11 +69,13 @@ class SentimentAlgorithm:
                         self.posDictTwoWords[twoWords] = self.posDictTwoWords.get(twoWords) + 1
                     else:
                         self.posDictTwoWords[twoWords] = 1
-            twoWords = '<>' + line[0].strip("'")
+            twoWords = '<s> ' + line[0].strip("'")
             if twoWords in self.posDictTwoWords:
                 self.posDictTwoWords[twoWords] = self.posDictTwoWords.get(twoWords) + 1
             else:
                 self.posDictTwoWords[twoWords] = 1
+        self.negDict['<s>'] = len(self.trainSentencesNeg)
+        self.negDict['</s>'] = len(self.trainSentencesNeg)
         for line in self.trainSentencesNeg:
             line = line.split()
             # print(line)
@@ -77,7 +87,7 @@ class SentimentAlgorithm:
                     self.negDict[word] = 1
                 if bigram:
                     if i == len(line) - 1:
-                        twoWords = word + ' <>'
+                        twoWords = word + ' </s>'
                     else:
                         twoWords = word + ' ' + line[i + 1].strip("'")
 
@@ -85,7 +95,7 @@ class SentimentAlgorithm:
                         self.negDictTwoWords[twoWords] = self.negDictTwoWords.get(twoWords) + 1
                     else:
                         self.negDictTwoWords[twoWords] = 1
-            twoWords = '<>' + line[0].strip("'")
+            twoWords = '<s> ' + line[0].strip("'")
             if twoWords in self.negDictTwoWords:
                 self.negDictTwoWords[twoWords] = self.negDictTwoWords.get(twoWords) + 1
             else:
@@ -101,16 +111,46 @@ class SentimentAlgorithm:
         for i in range(10):
             self.negDict.popitem()
 
+    def calc_pwi(self):
+        for key, value in self.posDict.items():
+            self.posPwi[key] = value/self.sumValuesPos
+
+        for key, value in self.negDict.items():
+            self.negPwi[key] = value/self.sumValuesNeg
+
+    def calc_p_wordpair(self):
+        for key, value in self.posDictTwoWords.items():
+            w1 = key.split()[0]
+            if w1 in self.posDict:
+                # print(key+"   "+w1)
+                self.posPpairwords[key] = value/self.posDict[w1]
+        for key, value in self.negDictTwoWords.items():
+            w1 = key.split()[0]
+            if w1 in self.negDict:
+                # print(key+"   "+w1)
+                self.negPpairwords[key] = value/self.negDict[w1]
+
+    def train(self):
+        self.calc_pwi()
+        self.calc_p_wordpair()
+
     def check_comment(self):
         return True
 
 
 if __name__ == '__main__':
     aiAgent = SentimentAlgorithm('rt-polarity.pos', 'rt-polarity.neg')
-    aiAgent.clean_dict()
+    aiAgent.train()
+    print(aiAgent.negPpairwords)
+    # print(aiAgent.posDictTwoWords)
+    # print(aiAgent.posDict)
+    # print(aiAgent.negDict)
 
-    print(aiAgent.posDict)
-    print(aiAgent.negDict)
+    # print(aiAgent.sumValuesPos)
+    # print(aiAgent.sumValuesNeg)
+    # print(aiAgent.posPwi)
+    # print(aiAgent.negPwi)
+
     # print(aiAgent.trainSentencesPos)
     # print(len(aiAgent.trainSentencesPos))
     # # print(aiAgent.testSentencesPos)
